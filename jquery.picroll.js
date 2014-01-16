638d7e5733d7e298ab5d6b4f7d6378c6626ca849
mingfunwong <mingfun.wong.chn@gmail.com>
2014-01-16 18:22:26 +0800

    Traversal DOM

--- jquery.picroll.js
+++ jquery.picroll.js
@@ -7,41 +7,34 @@
  */
 $.picroll = function(options) {
     var defaults = {
-        i: 5,
-        box: ".pic_roll",
-        content: ".content",
-        ul: "ul",
-        li: "li",
-        last: ".last",
-        next: ".next"
-    };  
-    var opts = $.extend(defaults, options);
+            i: 5,
+            box: ".pic_roll",
+            content: ".content",
+            ul: "ul",
+            li: "li",
+            last: ".last",
+            next: ".next"
+        },
+        opts = $.extend(defaults, options);
     
-    var i = opts.i;
-    var $box = $(opts.box);
-    var $content = $box.find(opts.content);
-    var $ul = $content.find(opts.ul);
-    var $li = $ul.find(opts.li);
-    var $last = $box.find(opts.last);
-    var $next = $box.find(opts.next);
-    var width = $content.parent().width();
-    var li_width = $li.width();
-    var now_page = 0;
-    var max_page = Math.ceil($li.length / i) - 1;
-    $last.on("click",
-        function(){
-            now_page --;
-            (now_page < 0) ? now_page = max_page : "";
-            $content.stop().animate({left: "-" + now_page * li_width * i}, "slow");
-       }
-    )
-    $next.on("click",
-        function(){
-            now_page ++;
-            (now_page > max_page) ? now_page = 0 : "";
-            $content.stop().animate({left: "-" + now_page * li_width * i}, "slow");
-       }
-    )
-  
+    $(opts.box).each(function (i){
+        var i = opts.i,
+            $box = $(this),
+            $content = $box.find(opts.content),
+            $ul = $content.find(opts.ul),
+            $li = $ul.find(opts.li),
+            width = $content.parent().width(),
+            li_width = $li.width(),
+            now_page = 0,
+            max_page = Math.ceil($li.length / i) - 1;
+        $box.on("click", opts.last, function(){
+                (--now_page < 0) ? now_page = max_page : "";
+                $content.stop().animate({left: "-" + now_page * li_width * i}, "slow");
+            })
+            .on("click", opts.next, function(){
+                (++ now_page > max_page) ? now_page = 0 : "";
+                $content.stop().animate({left: "-" + now_page * li_width * i}, "slow");
+        });
+    });
     return;
 }
