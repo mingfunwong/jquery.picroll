831892805df2e175af1ab293ceeef1b83b5aeb03
mingfunwong <mingfun.wong.chn@gmail.com>
2014-01-17 12:06:01 +0800

    Little code

--- jquery.picroll.js
+++ jquery.picroll.js
@@ -1,40 +1,43 @@
 /**
  * jquery.picroll
  * 
+ * $(".picroll").picroll({
+ *     i: 5,                // 每页显示数量
+ *     content: ".content", // ul 的父元素
+ *     ul: "ul",            // ul 元素
+ *     li: "li",            // li 元素
+ *     last: ".last",       // 上一页按钮
+ *     next: ".next",       // 下一页按钮
+ *     speed: "slow"        // 滚动速度
+ * });
+ * 
  * @link https://github.com/mingfunwong/jquery.picroll
  * @license http://opensource.org/licenses/MIT
  * @author Mingfun Wong <mingfun.wong.chn@gmail.com>
  */
-$.picroll = function(options) {
-    var defaults = {
+$.fn.picroll = function(options) {
+    var options = $.extend({
             i: 5,
-            box: ".pic_roll",
             content: ".content",
             ul: "ul",
             li: "li",
             last: ".last",
-            next: ".next"
-        },
-        opts = $.extend(defaults, options);
-    
-    $(opts.box).each(function (i){
-        var i = opts.i,
-            $box = $(this),
-            $content = $box.find(opts.content),
-            $ul = $content.find(opts.ul),
-            $li = $ul.find(opts.li),
-            width = $content.parent().width(),
-            li_width = $li.width(),
-            now_page = 0,
-            max_page = Math.ceil($li.length / i) - 1;
-        $box.on("click", opts.last, function(){
-                (--now_page < 0) ? now_page = max_page : "";
-                $content.stop().animate({left: "-" + now_page * li_width * i}, "slow");
+            next: ".next",
+            speed: "slow"
+        }, options);
+    this.each(function (i) {
+        var $box = $(this),
+            $content = $box.find(options.content),
+            $li = $content.find(options.ul).find(options.li),
+            width = $li.width(),
+            now = 0,
+            max = Math.ceil($li.length / options.i) - 1,
+            animate = function () { $content.stop().animate({left: now * width * options.i * -1}, options.speed) };
+        $box.on("click", options.last, function() {
+                animate(--now < 0 ? now = max : "")
             })
-            .on("click", opts.next, function(){
-                (++ now_page > max_page) ? now_page = 0 : "";
-                $content.stop().animate({left: "-" + now_page * li_width * i}, "slow");
+            .on("click", options.next, function(){
+                animate(++ now > max ? now = 0 : "")
         });
     });
-    return;
 }
