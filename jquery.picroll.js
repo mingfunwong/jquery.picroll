7b490485d3d61ffc492194a875a0f0aae410307c
mingfunwong <mingfun.wong.chn@gmail.com>
2014-04-03 18:36:23 +0800

    refine jquery.picroll add before,after

--- jquery.picroll.js
+++ jquery.picroll.js
@@ -9,6 +9,8 @@
  *     last: ".last",       // 上一页按钮
  *     next: ".next",       // 下一页按钮
  *     speed: "slow"        // 滚动速度
+ *     before: $.noop,      // 滚动前执行
+ *     after: $.noop        // 滚动后执行
  * });
  * 
  * @link https://github.com/mingfunwong/jquery.picroll
@@ -23,7 +25,9 @@ $.fn.picroll = function(options) {
             li: "li",
             last: ".last",
             next: ".next",
-            speed: "slow"
+            speed: "slow",
+            before: $.noop,
+            after: $.noop
         }, options);
     this.each(function (i) {
         var $box = $(this),
@@ -32,12 +36,15 @@ $.fn.picroll = function(options) {
             width = $li.width(),
             now = 0,
             max = Math.ceil($li.length / options.i) - 1,
-            animate = function () { width = $li.width(); $content.stop().animate({left: now * width * options.i * -1}, options.speed) };
+            animate = function () {options.before(now); width = $li.width(); $content.stop().animate({left: now * width * options.i * -1}, options.speed, function(){ options.after(now) }); };
         $box.on("click", options.last, function() {
                 animate(-- now < 0 ? now = max : "")
             })
             .on("click", options.next, function(){
                 animate(++ now > max ? now = 0 : "")
+            })
+            .on("select", function(event, i){
+                animate(now = i)
         });
     });
 }
